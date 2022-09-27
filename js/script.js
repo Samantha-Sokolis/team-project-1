77458bccec7fdd8bcb343d0777e0885e4368158c
asish-dewan <104346122+asish-dewan@users.noreply.github.com>
2022-09-28 00:05:04 +1000

    Added random word generate API function

--- js/script.js
+++ js/script.js
@@ -1,20 +1,20 @@
 // Initialise select options for search filter
-
+/* 
 document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('select');
     var instances = M.FormSelect.init(elems, options);
   });
-
+ */
   // Or with jQuery
 
   $(document).ready(function(){
     $('select').formSelect();
   });
 
-// About us tabs
-
+// About us tabs materialize animate
 $('.tabs').tabs();
 
+// Typing animation
 var typed = new Typed (".auto-type",{
     strings: ["brand names", "product names", "copy writing", "campaigns", "art projects"],
     typeSpeed: 40,
@@ -22,3 +22,50 @@ var typed = new Typed (".auto-type",{
     loop: true
 });
 
+// Fun fact API 
+
+var randomWordEl = $('#randomWord')
+var APIkey = "4s5jzik/flN/oJ+DRV00pg==FCTdV9mEsQVKjz4S";
+
+//
+
+function displayRandomWord(event){
+  event.preventDefault();
+  getFacts ();
+}
+
+function getFacts (){
+    var apiUrl = "https://api.api-ninjas.com/v1/randomword";
+    fetch(apiUrl)
+    .then (function (response) {
+            if (response.status == 200) {
+            console.log("This is working!" + response);
+            response.json().then (function (data) {
+                console.log (data);
+              
+              const {word} = data;
+            
+        // Display random word from response
+
+        $(randomWordEl).html(word);
+              })
+          }
+        })
+}
+
+getFacts();
+
+$("#random-button").on("click",displayRandomWord);
+/* $.ajax({
+  method: 'GET',
+  url: 'https://api.api-ninjas.com/v1/randomword',
+  headers: { 'X-Api-Key': '4s5jzik/flN/oJ+DRV00pg==FCTdV9mEsQVKjz4S'},
+  contentType: 'application/json',
+  success: function(result) {
+      console.log(result);
+  },
+  error: function ajaxError(jqXHR) {
+      console.error('Error: ', jqXHR.responseText);
+  }
+});
+ */
