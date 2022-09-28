dfc8f5d89f17cfaf604a29deed8bbeea7ade52a9
asish-dewan <104346122+asish-dewan@users.noreply.github.com>
2022-09-29 00:24:23 +1000

    Updated Random Word section with styling + added toast 'Abracadabra'

--- js/script.js
+++ js/script.js
@@ -1,10 +1,3 @@
-// Initialise select options for search filter
-
-document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('select');
-    var instances = M.FormSelect.init(elems, options);
-  });
-
   // Or with jQuery
 
   $(document).ready(function(){
