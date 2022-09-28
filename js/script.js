0716251e2688ef0a1bdb8a7505480dfc641d4736
Samantha <109199225+Samantha-Sokolis@users.noreply.github.com>
2022-09-28 18:05:22 +1000

    local storage to word input field added

--- js/script.js
+++ js/script.js
@@ -22,3 +22,20 @@ var typed = new Typed (".auto-type",{
     loop: true
 });
 
+// 
+//LOCAL STORAGE  - user enters word and clicks 'translate' - hero image goes away, and word saves to local
+var word = document.getElementById("translate")
+var saveButton = document.getElementById("save");
+
+function saveUserWord() {
+  var userWord = {
+    word: word.value.trim()
+  };
+  // Use .setItem() to store object in storage and JSON.stringify to convert it as a string
+  localStorage.setItem("userWord", JSON.stringify(userWord));
+}
+
+  saveButton.addEventListener("click", function(event) {
+    event.preventDefault();
+    saveUserWord();
+    });
